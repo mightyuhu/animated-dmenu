150ec5855d79c4bb5b150a36ff2050359b3817c3
syl20bnr <sylvain.benner@gmail.com>
2013-08-12 22:14:03 -0400

    Add history file patch

--- dmenu.c
+++ dmenu.c
@@ -75,10 +75,37 @@ static Window win;
 static XIC xic;
 static Bool fuzzy;
 static Bool returnearly = False;
+static char *histfile = NULL;
+static size_t histsize = 0;
 
 static int (*fstrncmp)(const char *, const char *, size_t) = strncmp;
 static char *(*fstrstr)(const char *, const char *) = strstr;
 static char *(*fstrchr)(const char *, const int) = strchr;
+
+
+static int
+writehistory(char *command) {
+	size_t i = 0;
+	FILE *f;
+
+	if(!histfile || strlen(command) <= 0)
+		return 0;
+
+	if((f = fopen(histfile, "w"))) {
+		fputs(command, f);
+		fputc('\n', f);
+		for(; i < histsize; i++) {
+			if(strcmp(command, items[i].text) != 0) {
+				fputs(items[i].text, f);
+				fputc('\n', f);
+			}
+		}
+		fclose(f);
+		return 1;
+	}
+
+	return 0;
+}
 
 int
 main(int argc, char *argv[]) {
@@ -133,6 +160,8 @@ main(int argc, char *argv[]) {
 			selbgcolor = argv[++i];
 		else if(!strcmp(argv[i], "-sf"))  /* selected foreground color */
 			selfgcolor = argv[++i];
+		else if(!strcmp(argv[i], "-hist"))
+			histfile = argv[++i];
 		else
 			usage();
 
@@ -226,32 +255,34 @@ drawmenu(void) {
 	dc->w = (lines > 0 || !matches) ? mw - dc->x : inputw;
 	drawtext(dc, text, normcol);
 	if((curpos = textnw(dc, text, cursor) + dc->font.height/2) < dc->w)
-		drawrect(dc, curpos, (dc->h - dc->font.height)/2 + 1, 1, dc->font.height - 1, True, normcol->FG);
+    drawrect(dc, curpos, (dc->h - dc->font.height)/2 + 1, 1, dc->font.height - 1, True, normcol->FG);
 
-    if(!quiet || strlen(text) > 0) {    
-        if(lines > 0) {
-            dc->w = mw - dc->x;
-            for(item = curr; item != next; item = item->right) {
-                dc->y += dc->h;
-                drawtext(dc, item->text, (item == sel) ? selcol : normcol);
-            }
-        }
-        else if(matches) {
-            dc->x += inputw;
-            dc->w = textw(dc, "<");
-            if(curr->left)
-                drawtext(dc, "<", normcol);
-            for(item = curr; item != next; item = item->right) {
-                dc->x += dc->w;
-                dc->w = MIN(textw(dc, item->text), mw - dc->x - textw(dc, ">"));
-                drawtext(dc, item->text, (item == sel) ? selcol : normcol);
-            }
-            dc->w = textw(dc, ">");
-            dc->x = mw - dc->w;
-            if(next)
-                drawtext(dc, ">", normcol);
-        }
+  if(!quiet || strlen(text) > 0) {
+    if(lines > 0) {
+      /* draw vertical list */
+      dc->w = mw - dc->x;
+      for(item = curr; item != next; item = item->right) {
+        dc->y += dc->h;
+        drawtext(dc, item->text, (item == sel) ? selcol : normcol);
+      }
     }
+    else if(matches) {
+      /* draw horizontal list */
+      dc->x += inputw;
+      dc->w = textw(dc, "<");
+      if(curr->left)
+        drawtext(dc, "<", normcol);
+      for(item = curr; item != next; item = item->right) {
+        dc->x += dc->w;
+        dc->w = MIN(textw(dc, item->text), mw - dc->x - textw(dc, ">"));
+        drawtext(dc, item->text, (item == sel) ? selcol : normcol);
+      }
+      dc->w = textw(dc, ">");
+      dc->x = mw - dc->w;
+      if(next)
+        drawtext(dc, ">", normcol);
+    }
+  }
 	mapdc(dc, win, mw, mh);
 }
 
@@ -408,6 +439,7 @@ keypress(XKeyEvent *ev) {
 	case XK_Return:
 	case XK_KP_Enter:
     handle_return((sel && !(ev->state & ShiftMask)) ? sel->text : text);
+		writehistory(sel ? sel->text : text);
 	case XK_Right:
 		if(text[cursor] != '\0') {
 			cursor = nextrune(+1);
@@ -566,20 +598,28 @@ paste(void) {
 void
 readstdin(void) {
 	char buf[sizeof text], *p, *maxstr = NULL;
-	size_t i, max = 0, size = 0;
-
-	/* read each line from stdin and add it to the item list */
-	for(i = 0; fgets(buf, sizeof buf, stdin); i++) {
-		if(i+1 >= size / sizeof *items)
-			if(!(items = realloc(items, (size += BUFSIZ))))
-				eprintf("cannot realloc %u bytes:", size);
-		if((p = strchr(buf, '\n')))
-			*p = '\0';
-		if(!(items[i].text = strdup(buf)))
-			eprintf("cannot strdup %u bytes:", strlen(buf)+1);
-		if(strlen(items[i].text) > max)
-			max = strlen(maxstr = items[i].text);
+	size_t i = 0, max = 0, size = 0;
+#define readstdin_internals(the_input_file) \
+	for(; fgets(buf, sizeof buf, the_input_file); i++) {\
+		if(i+1 >= size / sizeof *items)\
+			if(!(items = realloc(items, (size += BUFSIZ))))\
+				eprintf("cannot realloc %u bytes:", size);\
+		if((p = strchr(buf, '\n')))\
+			*p = '\0';\
+		if(!(items[i].text = strdup(buf)))\
+			eprintf("cannot strdup %u bytes:", strlen(buf)+1);\
+		if(strlen(items[i].text) > max)\
+			max = strlen(maxstr = items[i].text);\
+	}\
+	/* lines from the history file must appear first in menu */
+	FILE *f;
+	if(histfile && (f = fopen(histfile, "r"))) {
+		readstdin_internals(f);
+		histsize = i;
+		fclose(f);
 	}
+	/* read each line from stdin and add it to the item list */
+	readstdin_internals(stdin);
 	if(items)
 		items[i].text = NULL;
 	inputw = maxstr ? textw(dc, maxstr) : 0;
@@ -707,10 +747,11 @@ void handle_return(char* value) {
 
 void
 usage(void) {
-	fputs("usage: dmenu [-b] [-q] [-f] [-i] [-z] [-r] [-v]"
-        "             [-l lines]"
-        "             [-p prompt]"
+	fputs("usage: dmenu [-b] [-q] [-f] [-i] [-z] [-r] [-v]\n"
+        "             [-l lines]\n"
+        "             [-p prompt]\n"
         "             [-fn font]\n"
+        "             [-hist file]\n"
 	      "             [-x xoffset] [-y yoffset]\n"
         "             [-h height] [-w width]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color]\n", stderr);
